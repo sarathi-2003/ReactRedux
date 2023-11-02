28582fcf3d9eabc75901db58eadd2429c668fab0
sarathi-2003 <sarathip846@gmail.com>
2023-11-02 16:39:15 +0530

    change the value

--- src/Componenets/RootLayout.jsx
+++ src/Componenets/RootLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBarPanel";
 import { Provider } from "react-redux";
-import Store from "../Store/store";
+import Store from "../Store/Store";
 const RootLayout = () => {
   return (
     <>
